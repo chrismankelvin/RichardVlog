65fec1fc95df9287fb19b268a4f7b3d0d746e155
kelvinchrisman <chrismankelvin@gmail.com>
2023-07-20 23:11:36 +0000

    RichardVlogLive3

--- inc/database.inc.php
+++ inc/database.inc.php
@@ -1,10 +1,22 @@
-<?php
-          $dbservername = "localhost";
-          $username = "root";
-          $password = "";
-          $dbname = "richard_vlog";
+//<?php
+        //   $dbservername = "localhost";
+        //   $username = "root";
+        //   $password = "";
+        //   $dbname = "richard_vlog";
+
+// $connection = mysqli_connect($dbservername, $username, $password, $dbname);
+
+          $dbservername = "localhost:3306";
+          $username = "kervinklinsman_data";
+          $password = "9f70s$8gL";
+          $dbname = "kervinklinsman_database";
 
 $connection = mysqli_connect($dbservername, $username, $password, $dbname);
+
+
+
+
+
 
 // if (!$conn) {
 //     die("Connection failed: " . mysqli_connect_error());
