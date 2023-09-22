8b01270af9871020bd194029af3ab929c020f76d
chrismankelvin <chrismankelvin@gmail.com>
2023-09-22 15:50:58 +0000

    RichardVlog 7.6

--- inc/script.php
+++ inc/script.php
@@ -4,13 +4,7 @@ session_start();
 
 // Include any necessary database connection code
 // Replace with your actual database connection details
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "richard_vlog";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
+include'./database.inc.php';
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
